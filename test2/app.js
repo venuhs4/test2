6cbaa5025a8f7b4191e1a95b7eae6e1e38a322ed
Venu Prasad H S <venuhs4@gmail.com>
2016-05-12 18:08:48 +0530

    modified create server again

--- test2/app.js
+++ test2/app.js
@@ -4,4 +4,10 @@ var http = require('http');
 app.get('/data', function (req, res) {
     res.end('This is from server(Venu)' + new Date());
 })
-http.createServer(app).listen(8081);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
+http.createServer(app).listen(app.get('port') , app.get('ip'), function () {
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'), app.get('port'));
+    server();
+});
