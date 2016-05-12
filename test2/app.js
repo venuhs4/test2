0bc3bc82f22fb4e68e35168c3b8bf5826882eec3
Venu Prasad H S <venuhs4@gmail.com>
2016-05-12 17:55:36 +0530

    changed create server syntax

--- test2/app.js
+++ test2/app.js
@@ -1,1 +1,7 @@
-﻿console.log('Hello world');
+﻿var express = require('express');
+var app = express();
+var http = require('http');
+app.get('/data', function (req, res) {
+    res.end('This is from server(Venu)' + new Date());
+})
+http.createServer(app).listen(8081);
